7e9ebef74b54662cd0a64749667533237ac6c15b
joseph-benoy <josephbenoy03@gmail.com>
2021-03-12 20:23:54 +0530

    "backup updated - restore dest added"

--- backup.sh
+++ backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#backup function
 function back(){
     mkdir -p /home/$USER/.back_util
     dir_to_back=$1
@@ -6,19 +8,45 @@ function back(){
     if [ $len -eq 0 ]
     then
         today=`date "+%Y-%m-%d_%T"`
+        echo $PWD>restore_destination_file
         echo "Directory to backup : $PWD"
         echo "Backing up......"
         tar cvfz /home/$USER/.back_util/backup_arch_$today.tgz *
         echo "Backup finshed!"
         echo -n "Backup size : "
         wc -c</home/$USER/.back_util/backup_arch_$today.tgz
+        rm restore_destination_file
     else
+    	readlink -f $dir_to_back>$dir_to_back/restore_destination_file
         today=`date "+%Y-%m-%d_%T"`
-        echo "Directory to backup : $dir_to_back"
+        echo "Directory to backup : `readlink -f $dir_to_back`"
         echo "Backing up......"
         tar cvfz /home/$USER/.back_util/backup_arch_$today.tgz $dir_to_back
         echo "Backup finshed!"
         echo -n "Backup size : "
         wc -c</home/$USER/.back_util/backup_arch_$today.tgz
+        rm $dir_to_back/restore_destination_file
     fi
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#restore from backup function
+function restore(){
+	ls /home/$USER/.back_util
 }
