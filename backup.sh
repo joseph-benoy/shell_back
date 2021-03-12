4eca7b82adc6d5dd19803cb0f4b5097e164c616a
joseph-benoy <josephbenoy03@gmail.com>
2021-03-12 21:12:54 +0530

    restore function added

--- backup.sh
+++ backup.sh
@@ -1,52 +1,29 @@
 #!/bin/bash
-
 #backup function
 function back(){
     mkdir -p /home/$USER/.back_util
-    dir_to_back=$1
-    len=${#dir_to_back}
-    if [ $len -eq 0 ]
-    then
-        today=`date "+%Y-%m-%d_%T"`
-        echo $PWD>restore_destination_file
-        echo "Directory to backup : $PWD"
-        echo "Backing up......"
-        tar cvfz /home/$USER/.back_util/backup_arch_$today.tgz *
-        echo "Backup finshed!"
-        echo -n "Backup size : "
-        wc -c</home/$USER/.back_util/backup_arch_$today.tgz
-        rm restore_destination_file
-    else
-    	readlink -f $dir_to_back>$dir_to_back/restore_destination_file
-        today=`date "+%Y-%m-%d_%T"`
-        echo "Directory to backup : `readlink -f $dir_to_back`"
-        echo "Backing up......"
-        tar cvfz /home/$USER/.back_util/backup_arch_$today.tgz $dir_to_back
-        echo "Backup finshed!"
-        echo -n "Backup size : "
-        wc -c</home/$USER/.back_util/backup_arch_$today.tgz
-        rm $dir_to_back/restore_destination_file
-    fi
+    today=`date "+%Y-%m-%d_%T"`
+    echo $PWD>restore_destination_file
+    echo "Directory to backup : $PWD"
+    echo "Backing up......"
+    tar cvfz /home/$USER/.back_util/backup_arch_$today.tgz *
+    echo "Backup finshed!"
+    echo -n "Backup size : "
+    wc -c</home/$USER/.back_util/backup_arch_$today.tgz
+    rm restore_destination_file
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #restore from backup function
 function restore(){
-	ls /home/$USER/.back_util
+    ls /home/$USER/.back_util
+    echo -n "Enter the name of the backup file : "
+    read file_name
+    echo "Restoring......"
+    tar xvfz /home/$USER/.back_util/$file_name restore_destination_file
+    destination=`cat restore_destination_file`
+    mkdir -p $destination
+    rm restore_destination_file
+    tar xvfz /home/$USER/.back_util/$file_name -C $destination
+    rm $destination/restore_destination_file
+    echo "Restoration completed!"
 }
